32aa4bf68e28a9d01c7d7fd1e147a4253bee57bc
Aziz Ulmasov <azizulmasov93@gmail.com>
2023-07-05 21:19:57 +0500

    task completed

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+        for (int i = 1; i <= sideLength ; i++) {
+            System.out.print(8);
+            for (int j = 1; j <= sideLength - 2; j++) {
+                if (i == 1 || i == sideLength) System.out.print(8);
+                else System.out.print(" ");
+            }
+            System.out.println(8);
+        }
     }
 }
